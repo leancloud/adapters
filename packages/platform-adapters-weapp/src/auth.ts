ddd1a4e5312020a1737e0923d84e40be76877709
sdjdd <i@sdjdd.com>
2020-04-28 15:06:11 +0800

    feat: implement weapp getAuthInfo function

--- packages/platform-adapters-weapp/src/auth.ts
+++ packages/platform-adapters-weapp/src/auth.ts
@@ -1,19 +1,33 @@
-import { Adapters } from "@leancloud/adapter-types";
+import { AuthData } from "@leancloud/adapter-types";
 import "miniprogram-api-typings";
 
-const PLATFORM = "lc_weapp";
+const PROVIDER = "lc_weapp";
+const PLATFORM = "weixin";
 
 function getLoginCode(): Promise<string> {
-    return new Promise((resolve, reject) => {
-        wx.login({
-            success: res =>
-                res.code ? resolve(res.code) : reject(new Error(res.errMsg)),
-            fail: ({errMsg}) => reject(new Error(errMsg))
-        });
+  return new Promise((resolve, reject) => {
+    wx.login({
+      success: res =>
+        res.code ? resolve(res.code) : reject(new Error(res.errMsg)),
+      fail: ({ errMsg }) => reject(new Error(errMsg))
     });
+  });
 }
 
-export const getAuthData: Adapters["getAuthData"] = async function() {
-    let code = await getLoginCode();
-    return [PLATFORM, { code }];
+export const getAuthInfo = async function ({
+  platform = PLATFORM,
+  preferUnionId = false,
+  asMainAccount = false,
+} = {}) {
+  const code = await getLoginCode();
+  let authData: AuthData = { code };
+  if (preferUnionId) {
+    authData.platform = platform;
+    authData.main_account = asMainAccount;
+  }
+  return {
+    authData,
+    platform,
+    provider: PROVIDER,
+  };
 }
