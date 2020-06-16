a110148107258fee1c5ad4c19990fce11a2ae7b3
sdjdd <i@sdjdd.com>
2020-06-16 17:34:29 +0800

    feat(toutiao): add anonymousCode to authData

--- packages/platform-adapters-toutiao/src/auth.ts
+++ packages/platform-adapters-toutiao/src/auth.ts
@@ -1,23 +1,18 @@
 const PROVIDER = "lc_tt";
 
-function getLoginCode(force: boolean): Promise<string> {
-  return new Promise((resolve, reject) => {
-    tt.login({
-      force,
-      success: (res: any) => resolve(res.code),
-      fail: (res: any) => reject(new Error(res.errMsg)),
-    });
-  });
-}
-
-export async function getAuthInfo({
+export function getAuthInfo({
   force = false,
 }: {
   force?: boolean;
 } = {}) {
-  const code = await getLoginCode(force);
-  return {
-    authData: { code },
-    provider: PROVIDER,
-  };
+  return new Promise(function(resolve, reject) {
+    tt.login({
+      force,
+      success: (res: any) => resolve({
+        authData: { code: res.code, anonymousCode: res.anonymousCode },
+        provider: PROVIDER,
+      }),
+      fail: (res: any) => reject(new Error(res.errMsg)),
+    });
+  })
 }
