f1edfe4438ffe3dceba7ea2b18163baa1eca4ac5
sdjdd <i@sdjdd.com>
2020-07-28 18:01:57 +0800

    feat: toutiao and weapp is ok

--- packages/platform-adapters-toutiao/src/websocket.ts
+++ packages/platform-adapters-toutiao/src/websocket.ts
@@ -1,5 +1,5 @@
 import { Adapters } from "@leancloud/adapter-types";
-import { WS } from "@leancloud/adapter-utils";
+import { WS } from "@leancloud/adapter-utils/lib/esm";
 
 class ToutiaoWS extends WS {
   private _socketTask: ToutiaoMiniApp.SocketTask;
