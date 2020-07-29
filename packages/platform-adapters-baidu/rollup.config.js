3c93b48d3bfa085cdbbc45568bc852294a4c5cab
sdjdd <i@sdjdd.com>
2020-07-29 12:42:10 +0800

    build(baidu): remove unnecessary commonjs plugin

--- packages/platform-adapters-baidu/rollup.config.js
+++ packages/platform-adapters-baidu/rollup.config.js
@@ -1,6 +1,5 @@
 import typescript from "@rollup/plugin-typescript";
 import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
 
 export default {
   input: "src/index.ts",
@@ -14,6 +13,5 @@ export default {
       tsconfig: "tsconfig.build.json",
     }),
     resolve(),
-    commonjs(),
   ],
 }
