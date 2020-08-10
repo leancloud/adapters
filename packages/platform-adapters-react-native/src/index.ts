d63b57b25e22da8bd129abc784d05a009ae238bf
Lee Yeh <lee55962698@gmail.com>
2020-08-10 18:19:22 +0800

    feat(RN): downgrade @react-native-community/async-storage to v1 (#17)
    to make it work with Expo(v38)

--- packages/platform-adapters-react-native/src/index.ts
+++ packages/platform-adapters-react-native/src/index.ts
@@ -1,29 +1,9 @@
 import { Adapters } from "@leancloud/adapter-types";
 export * from "@leancloud/platform-adapters-browser";
 
-import LegacyStorage from "@react-native-community/async-storage-backend-legacy";
-import AsyncStorageFactory, {
-  AsyncStorage,
-} from "@react-native-community/async-storage";
+import AsyncStorage from '@react-native-community/async-storage';
 
-type StorageModel = { [key in symbol | number | string]: string };
-
-const defaultLegacyStorage = AsyncStorageFactory.create(
-  new LegacyStorage<StorageModel>(),
-  {}
-);
-
-const createStorage = (
-  asyncStorage: AsyncStorage<StorageModel, any>
-): Adapters["storage"] => ({
-  async: true,
-  getItem: asyncStorage.get.bind(asyncStorage),
-  setItem: asyncStorage.set.bind(asyncStorage),
-  removeItem: asyncStorage.remove.bind(asyncStorage),
-  clear: asyncStorage.clearStorage.bind(asyncStorage),
-});
-
-export const storage = createStorage(defaultLegacyStorage);
+export const storage = {...AsyncStorage, async: true };
 
 export const platformInfo: Adapters["platformInfo"] = {
   name: "ReactNative",
